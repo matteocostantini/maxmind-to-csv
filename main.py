a597b8c77918bba8866e938465f15616b5e304a6
Area Sviluppo <matteo.costantini@gmail.com>
2023-05-02 15:53:47 +0200

    fix continent resolve

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import csv
 import maxminddb
 
 # Path al file che contiene i dati del paese associati agli indirizzi IP
-mmdb_file_path = "GeoLite2-Country.mmdb"
-
+mmdb_file_path = "GeoLite2-City.mmdb"
 
 def get_continent(ip_address, reader):
     try:
@@ -36,6 +35,22 @@ def get_country(ip_address, reader):
     except KeyError:
         return "N/A"
 
+# Funzione che restituisce il paese associato all'indirizzo IP
+def get_city(ip_address, reader):
+    try:
+        response = reader.get(ip_address)    
+    except ValueError:
+        return "N/A"
+    
+    try:
+        #response = reader.get(ip_address)
+        #response = reader.country(ip_address)
+        #response = reader.get(ip_address)
+        #country = response.country
+        return response['city']['names']['en']
+    except KeyError:
+        return "N/A"
+
 # Apri il file CSV con gli indirizzi IP
 with open("indirizzi_ip.csv") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
@@ -46,7 +61,7 @@ with open("indirizzi_ip.csv") as csv_file:
 reader = maxminddb.open_database(mmdb_file_path)
 
 # Crea una lista di tuple con l'indirizzo IP e il paese associato
-country_list = [(ip_address, get_continent(ip_address, reader), get_country(ip_address, reader)) for ip_address in ip_list]
+country_list = [(ip_address, get_continent(ip_address, reader), get_country(ip_address, reader), get_city(ip_address, reader)) for ip_address in ip_list]
 
 # Chiudi il database MaxMindDB
 reader.close()
@@ -54,6 +69,6 @@ reader.close()
 # Scrivi i risultati su un nuovo file CSV
 with open("indirizzi_ip_con_paese.csv", mode="w", newline="") as csv_file:
     writer = csv.writer(csv_file, delimiter=",")
-    writer.writerow(["Indirizzo IP", "Continente", "Paese"])
-    for ip_address, continent, country in country_list:
-        writer.writerow([ip_address, continent, country])
+    writer.writerow(["IP_Address", "Continent", "Country", "City"])
+    for ip_address, continent, country, city in country_list:
+        writer.writerow([ip_address, continent, country, city])
