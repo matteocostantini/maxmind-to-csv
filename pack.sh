1f3dc8fa10d1a96f87a959fe9720715f29eb063d
Area Sviluppo <matteo.costantini@gmail.com>
2023-05-05 14:09:11 +0200

    fixes

--- pack.sh
+++ pack.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 rm -rf dist
 pyinstaller --clean -y -F  -n MaxMindDB-to-CSV -i logo.ico main.py
-cp config.yaml.example dist/config.yaml
+cp config.yaml dist/config.yaml
 cp IP.csv dist/.
 cp tools/geoip_convert-v2-v1.sh dist/geoip_convert-v2-v1.sh
 #/usr/share/GeoIP/GeoIP.dat
+bash dist/MaxMindDB-to-CSV --update
